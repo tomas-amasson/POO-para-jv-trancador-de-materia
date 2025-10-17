5233ff9336402643010b53e812c3f48c7aa4ec5a
Tomás Amarante Masson <massontomas18@gmail.com>
2025-10-17 14:41:24 -0300

    Update fracao.cpp

--- fracao.cpp
+++ fracao.cpp
@@ -30,12 +30,22 @@ void Fracao::set_numerador(int num)
     numerador = num;
 }
 
-Fracao mult(Fracao f1, Fracao f2)
+Fracao operator*(Fracao f1, Fracao f2)
 {
     Fracao f3;
 
     f3.set_denominador(f1.get_denominador() * f2.get_denominador());
     f3.set_numerador(f1.get_numerador() * f2.get_numerador());
+
+    return f3;
+}
+
+Fracao Fracao::mult(Fracao f2)
+{
+    Fracao f3;
+
+    f3.set_denominador(get_denominador() * f2.get_denominador());
+    f3.set_numerador(get_numerador() * f2.get_numerador());
 
     return f3;
 }
